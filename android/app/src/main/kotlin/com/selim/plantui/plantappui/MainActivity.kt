5820a791d170eeccd1dff1119c86788a1d6756f6
ansSelim <ansselim6@gmail.com>
2022-03-29 21:33:29 +0200

    project setup and add constants

---
+++ android/app/src/main/kotlin/com/selim/plantui/plantappui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.selim.plantui.plantappui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Plant App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+
+    );
+  }
+}
+
